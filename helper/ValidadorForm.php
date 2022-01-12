8aebda0c5c34ebae9a8319463bc7d450ed6f4e84
Mikel Goicoechea - DAW 2 <56249182+mklgt@users.noreply.github.com>
2022-01-12 09:46:07 +0100

    Actualización de ValidadorForm
    Actualización por un error en un git push del 11/01/2022

--- helper/ValidadorForm.php
+++ helper/ValidadorForm.php
@@ -17,24 +17,16 @@ class ValidadorForm
             if (isset($fuente[$campo])) {
                 $campoAValidar = $fuente[$campo];
                 foreach ($valores as $regla => $valorRegla) {
-<<<<<<< HEAD
 
                     switch ($regla) {
                         case 'required':
 
-=======
-                    
-                    switch ($regla) {
-                        case 'required':
-                            echo "<br>El campo a validar: $campoAValidar ". strlen($campoAValidar);
->>>>>>> 75d28b9739d6e9f2b803ed6a8287fcdfafa56765
                             if (is_null($campoAValidar) || empty($campoAValidar)) {
                                 $this->addError($campo, "Es requerido");
                             }
                             break;
 
                         case 'min':
-<<<<<<< HEAD
                             if (strtotime($campoAValidar)) {
                                 if (strtotime($campoAValidar) < strtotime($valorRegla)) {
                                     $this->addError($campo, "Debe ser mayor que " . $valorRegla);
@@ -43,15 +35,10 @@ class ValidadorForm
                                 if (strlen($campoAValidar) < $valorRegla) {
                                     $this->addError($campo, "Debe ser mayor que " . $valorRegla);
                                 }
-=======
-                            if (strlen($campoAValidar) < $valorRegla || $campoAValidar < $valorRegla) {
-                                $this->addError($campo, "Debe ser mayor que " . $valorRegla);
->>>>>>> 75d28b9739d6e9f2b803ed6a8287fcdfafa56765
                             }
                             break;
 
                         case 'max':
-<<<<<<< HEAD
                             if (strtotime($campoAValidar)) {
                                 if (strtotime($campoAValidar) > strtotime($valorRegla)) {
                                     echo "$campoAValidar > $valorRegla";
@@ -61,10 +48,6 @@ class ValidadorForm
                                 if (strlen($campoAValidar) > $valorRegla) {
                                     $this->addError($campo, "Debe ser menor que " . $valorRegla);
                                 }
-=======
-                            if (strlen($campoAValidar) > $valorRegla) {
-                                $this->addError($campo, "Debe ser menor que " . $valorRegla);
->>>>>>> 75d28b9739d6e9f2b803ed6a8287fcdfafa56765
                             }
                             break;
 
@@ -80,33 +63,22 @@ class ValidadorForm
                             $this->addError($campo, "tiene que tener más de 8 carácteres");
                         }
                     }*/
-<<<<<<< HEAD
                 }
             } else {
                 $this->addError($campo, "Es requerido");
-=======
-
-                }
-            } else {
-                $this->addError($campo, "requerido");
->>>>>>> 75d28b9739d6e9f2b803ed6a8287fcdfafa56765
             }
         }
     }
 
     public function addError($nombreCampo, $error)
     {
-<<<<<<< HEAD
         //$this->errores[] = "El campo " . $nombreCampo . " : " . $error;
-=======
->>>>>>> 75d28b9739d6e9f2b803ed6a8287fcdfafa56765
         $this->errores[] = "El campo " . $nombreCampo . " : " . $error;
     }
 
     public function getErrores()
     {
         return $this->errores;
-<<<<<<< HEAD
     }
 
     public function getMensajeError($campo)
@@ -121,8 +93,6 @@ class ValidadorForm
         if (isset($this->errores[$campo])) {
             return $this->errores[$campo];
         }*/
-=======
->>>>>>> 75d28b9739d6e9f2b803ed6a8287fcdfafa56765
     }
 
     public function esValido()
